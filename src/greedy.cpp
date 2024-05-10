3ee7e936ec60d53ae33aa98610f8ee49d014d43b
Hugo Sánchez <hugosanchezmartinez4@gmail.com>
2024-05-10 21:03:08 +0200

    Update greedy.cpp

--- src/greedy.cpp
+++ src/greedy.cpp
@@ -4,14 +4,13 @@
 
 using namespace std;
 
-// TDA Solución compuesto por la máxima diversidad y
-// el subconjunto que la forma
+/* TAD Solución */
 struct Solucion {
     int diversidad;
     set<int> subset;
 };
 
-// Lee las diversidades de cada elemento y las guarda en una matriz
+/* Lee las diversidades de cada elemento y las guarda en una matriz */
 void readDistances(int n, vector<vector<int>>& distances) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
@@ -20,7 +19,7 @@ void readDistances(int n, vector<vector<int>>& distances) {
     }
 }
 
-// Imprime la suma máxima y el subconjunto solución
+/* Imprime la suma máxima y el subconjunto solución */
 void printSolution(int n, Solucion s) {
     cout << s.diversidad << endl;
     for (int i = 0; i < n; ++i) {
@@ -47,13 +46,13 @@ Solucion greedy(int n, int m, const vector<vector<int>> distances) {
     int maxSum = 0;
     set<int> maxSubset;
 
-    // Iterar sobre todos los subconjuntos posibles de tamaño m
+    /* Iterar sobre todos los subconjuntos posibles de tamaño m */
     for (int subset = 0; subset < (1 << n); ++subset) {
         if (__builtin_popcount(subset) == m) {
             int sum = 0;
             set<int> selectedIndices;
 
-            // Calcular la suma de distancias para este subconjunto
+            /* Calcular la suma de distancias para este subconjunto */
             for (int i = 0; i < n; ++i) {
                 if (subset & (1 << i)) {
                     selectedIndices.insert(i);
@@ -64,7 +63,7 @@ Solucion greedy(int n, int m, const vector<vector<int>> distances) {
                     }
                 }
             }
-            // Actualizar si esta suma es mayor que la máxima encontrada hasta ahora
+            /* Actualizar si esta suma es mayor que la máxima encontrada hasta ahora */
             if (sum > maxSum) {
                 maxSum = sum;
                 maxSubset = selectedIndices;
@@ -86,7 +85,7 @@ int main() {
         int n, m;
         cin >> n >> m;
 
-        // Matriz de distancias
+        /* Matriz de distancias */
         vector<vector<int>> distances(n, vector<int>(n));
         readDistances(n, distances);
         
